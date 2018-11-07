a6db375983c16314797808327a5fb7ec411254e3
Hannah <hannah_farjami@hotmail.com>
2018-11-07 17:36:15 +0100

    Added Word creator and functionality.

--- src/server/model/Game.java
+++ src/server/model/Game.java
@@ -1,4 +1,26 @@
 package server.model;
+import server.util.WordGenerator;
 
+/**
+ * This is the Game class that handles all the game logic for the Hangman game
+ */
 public class Game {
+
+    private String word;
+    private int failedAttempts;
+    private String theWordSoFar[];
+    private int totalScore;
+
+    /**
+     * creates an instance
+     */
+    public Game() {
+    }
+
+    public void newGame(){
+        pickAWord();
+    }
+    public void pickAWord() {
+        this.word = WordGenerator.getWord();
+    }
 }
